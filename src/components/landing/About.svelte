b94a871fbf39e950ca261654b9414ca0fbdb333b
Schmarj3 <phone.mfe@gmail.com>
2024-11-10 10:01:21 -0500

    tmp rm until dynamically set from sanity

--- src/components/landing/About.svelte
+++ src/components/landing/About.svelte
@@ -1,23 +1,29 @@
 <script>
-    import { Col, Container, Row } from "@sveltestrap/sveltestrap";
+	import { Col, Container, Row } from '@sveltestrap/sveltestrap';
 </script>
+
 <section class="section" id="about">
-    <Container>
-        <Row class="align-items-center">
-            <Col lg="6">
+	<Container>
+		<Row class="align-items-center">
+			<!-- <Col lg="6">
                 <div class="mt-3">
                     <img src="assets/images/slopes.jpg" alt="" class="img-fluid mx-auto d-block img-thumbnail">
                 </div>
-            </Col>
-            <Col lg="6">
-                <div class="mt-3">
-                    <h4 class="mt-4"><span class="text-primary fw-bold">Welcome!</span></h4>
-                    <p class="text-muted mt-4">I strongly believe in the power of learning to reinvent oneself. I write content about what I am learning and my reflections on life with the goal of bringing value to others. </p>
-                    <p class="text-muted mt-2">Thanks for stopping by, and I hope you found something of value here before you go.</p>
-                    <p class="text-muted mt-2">Blessings and best wishes to you and yours,</p>
-                    <p class="text-muted mt-2"><em>-m.etienne</em></p>
-                </div>
-            </Col>
-        </Row>
-    </Container>
+            </Col> -->
+			<Col lg="6">
+				<div class="mt-3">
+					<h4 class="mt-4"><span class="text-primary fw-bold">Welcome!</span></h4>
+					<p class="text-muted mt-4">
+						I strongly believe in the power of learning to reinvent oneself. I write content about
+						what I am learning and my reflections on life with the goal of bringing value to others.
+					</p>
+					<p class="text-muted mt-2">
+						Thanks for stopping by, and I hope you found something of value here before you go.
+					</p>
+					<p class="text-muted mt-2">Blessings and best wishes to you and yours,</p>
+					<p class="text-muted mt-2"><em>-m.etienne</em></p>
+				</div>
+			</Col>
+		</Row>
+	</Container>
 </section>
