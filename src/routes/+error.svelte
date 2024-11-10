5b2061e7d71c4fd78eba58a3d3c95d803a2995b8
Schmarj3 <phone.mfe@gmail.com>
2024-11-10 12:31:16 -0500

    error page, fix adapter config

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.error}
+	<h1>{$page.error.message}</h1>
+{:else}
+	<h1>Yikes! An unknown error has occured</h1>
+{/if}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,16 @@
 import { mostRecentPostsQuery as query, type Post } from '$lib/sanity/queries';
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async (event) => {
 	const { loadQuery } = event.locals;
 	const initial = await loadQuery<Post[]>(query);
+
+	if (!initial) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
 
 	// We pass the data in a format that is easy for `useQuery` to consume in the
 	// corresponding `+page.svelte` file, but you can return the data in any
